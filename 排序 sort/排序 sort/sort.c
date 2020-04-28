41e0a54c60e90798f2b99a97178072ba89a39526
sxd422638 <1623130592@qq.com>
2020-04-28 21:09:10 +0800

    sort

--- 排序 sort/排序 sort/sort.c
+++ 排序 sort/排序 sort/sort.c
@@ -20,6 +20,7 @@ void PrintArray(int* a, int n)//打印
 void InsertSort(int* a, int n)//    再去控制多趟排序
 {								
 	//先写单趟排序 将tmp插入[0 , end]有序区间，依旧保持有序
+
 	for (size_t i = 0; i < n - 1 ; ++i)
 	{
 		int end = i ;
@@ -165,6 +166,216 @@ void HeapSort(int* a, int n)//堆排序
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//5.冒泡排序
+void BubbleSort(int* a, int n)
+{
+	int end = n;
+	while (end > 0)
+	{
+		int exchange = 0;
+		//单趟冒泡
+		for (int i = 1; i < end; ++i)
+		{
+			if (a[i - 1] > a[i])
+			{
+				Swap(&a[i - 1], &a[i]);
+				exchange = 1;
+			}
+		}
+		if (exchange == 0)
+			break;
+		--end;//控制end的终止位置
+	}
+}
+
+//6.分制排序 相当于单趟的快速排序
+
+
+
+
+
+//三数取中 把中间数的下标找出来 (一种优化的方式)
+int GetMidIndex(int* a, int begin, int end)
+{
+	int mid = (begin + end) / 2;
+
+	if (a[begin] < a[mid])
+	{
+		if (a[mid] < a[end])
+		{
+			return mid;
+		}
+		else if (a[begin] > a[end])
+		{
+			return begin;
+		}
+		else
+		{
+			return end;
+		}
+	}
+	else// a[begin] > a[mid]
+	{
+
+		if (a[mid] > a[end])
+		{
+			return mid;
+		}
+		else if (a[begin] < a[end])
+		{
+			return begin;
+		}
+		else
+		{
+			return end;
+		}
+	}
+
+}
+
+
+
+
+
+//[begin , end ]左边去做key
+//int PartSort(int* a, int begin , int end )
+//{
+//	
+//	int key = begin;
+//	while (begin < end)
+//	{
+//		//end先走 找小
+//		while (begin < end && a[end] >= a[key])//a[end] < a[key]的时候停下来
+//		{
+//			--end;
+//		}
+//		//begin走 找大
+//		while (begin < end && a[begin] <= a[key])//a[end] < a[key]的时候停下来
+//		{
+//			++begin;
+//		}
+//		Swap(&a[begin], &a[end]);
+//	}
+//	Swap(&a[key], &a[begin]);
+//	return begin;
+//}
+
+//右边去做key
+
+int PartSort1(int* a, int begin, int end)
+{
+	int mid = GetMidIndex(a, begin, end);
+	Swap(&a[mid], &a[end]);
+	int key = end;
+	while (begin < end)
+	{
+		//begin先走 找大
+		while (begin < end && a[begin] <= a[key])//a[end] < a[key]的时候停下来
+		{
+			++begin;
+		}
+		Swap(&a[begin], &a[end]);
+		//end后走 找小
+		while (begin < end && a[end] >= a[key])//a[end] < a[key]的时候停下来
+		{
+			--end;
+		}
+		
+	}
+	Swap(&a[key], &a[begin]);
+	return begin;
+}
+
+
+
+
+//2.挖坑法
+int PartSort2(int* a, int begin, int end)
+{
+	int mid = GetMidIndex(a, begin, end);
+	Swap(&a[mid], &a[begin]);
+	int key = a[begin];
+	while (begin < end)
+	{
+		while (begin < end && a[end] >= key)
+		{
+			--end;
+		}
+		a[begin] = a[end];//begin是第一个坑位
+
+		while (begin < end && a[begin] <= key)
+		{
+			++begin;
+		}
+		a[end] = a[begin];
+	}
+	a[begin] = key;
+	return begin;
+	
+}
+
+//3.前后指针法
+int PartSort2(int* a, int begin, int end)
+{
+	int prev = begin - 1;
+	int cur = begin;
+	int key = end;
+	while (cur < end)
+	{
+		if (a[cur] < a[key])//if(a[cur] < a[key] && ++prve != cur )
+		{
+			++prev;
+			Swap(&a[prev], &a[cur]);
+		}
+			++cur;
+	}
+	++prev;
+	Swap(&a[prev], &a[key]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//快速排序(递归)
+void QuickSort(int* a, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	if ((right - left + 1) < 10)//表示个数
+	{
+		InesrtSort( a+left ,  right - left + 1);
+
+	}
+
+		int keyIndex = PartSort(a, left, right);
+		//[left , keyindex-1] [keyindex] [keyindex , right]
+		QuickSort(a, left, keyIndex - 1);
+		QuickSort(a, keyIndex + 1, right);
+	
+ }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 排序 sort/排序 sort/sort.h
+++ 排序 sort/排序 sort/sort.h
@@ -16,3 +16,13 @@ void SelectSort(int* a, int n);//直接选择排序
 
 
 void HeapSort(int* a, int n);//堆排序
+
+void BubbleSort(int* a, int n);//冒泡排序
+
+int PartSort(int* a, int begin , int end);//分制排序
+int PartSort2(int* a, int begin, int end);//2.挖坑法
+
+int PartSort2(int* a, int begin, int end); //3.前后指针法
+
+int GetMidIndex(int* a, int left, int right);//三数取中(快排优化)
+void QuickSort(int* a, int left, int right);//快速排序
